2e8a008f9f32430e254c671a6390e96c38fc229a
notthatzurrz-cmyk <notthatzurrz@gmail.com>
2026-09-04 11:31:11 -0700

    Update yuno_loader.lua

--- yuno_loader.lua
+++ yuno_loader.lua
@@ -1,17 +1,1 @@
---[[
-    ===================================================================
-    YUNO SCRIPT LOADER (PLATOBOOST KEY SYSTEM INTEGRATION)
-    ===================================================================
-    Service ID: 30793
-    HMAC Key: c5f0932b-3cdb-4dca-b2a3-5bdc958bca23
-    Get Key Link: https://gateway.platoboost.com/a?id=30793
-    
-    Usage:
-    key = "secretkey"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/notthatzurrz-cmyk/yuno/refs/heads/main/yuno%201.txt"))()
-    ===================================================================
---]]
-
-key = key or getgenv().key or _G.key
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notthatzurrz-cmyk/yuno/refs/heads/main/yuno%201.txt"))()
+dummy u thought
